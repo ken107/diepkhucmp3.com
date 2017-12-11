e85adb0308311ac7a824008983ab1e78178a920d
Hai Phan <hai.phan@gmail.com>
2017-12-11 14:42:02 -0800

    cool

--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,10 @@
 
+recording = null;
+
+function submit(chunks) {
+  ajaxPut("http://localhost:30112/put", new Blob(chunks));
+}
+
 function record() {
   return getUserMedia({
     "audio": {
@@ -11,10 +17,10 @@ function record() {
       "optional": []
     },
   })
-  .then(function(stream) {
+  .then(function(webcam) {
     var audioContext = new AudioContext();
-    var source = audioContext.createMediaStreamSource(stream);
-    var capture = audioContext.createScriptProcessor(16384, 1, 0);
+    var source = audioContext.createMediaStreamSource(webcam);
+    var capture = audioContext.createScriptProcessor(16384, 1, 1);
     var chunks = [];
     capture.onaudioprocess = function(event) {
       chunks.push(event.inputBuffer.getChannelData(0));
@@ -23,10 +29,25 @@ function record() {
     capture.connect(audioContext.destination);
     return {
       stop: function() {
+        webcam.getTracks().forEach(function(track) {track.stop()});
         source.disconnect();
         capture.disconnect();
         return chunks;
       }
     }
+  })
+}
+
+function ajaxPut(sUrl, oData) {
+  return new Promise(function(fulfill, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", sUrl, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        if (xhr.status == 200) fulfill(xhr.responseText);
+        else reject(new Error(xhr.responseText));
+      }
+    };
+    xhr.send(oData);
   })
 }
